65a27e0d03293dd1fabb567bfede67b960462190
Leo Brewin <Leo.Brewin@monash.edu>
2020-08-15 13:22:55 +1000

    Trivial housekeeping.
    Updated Makefiles to reference "cputime" rather than "cdbstats".

--- source/cadabra/example-06.sh
+++ source/cadabra/example-06.sh
@@ -2,7 +2,7 @@
 
 echo "cdblatex.sh example-06 ..."
 cdblatex.sh -T -skx -i example-06 &> example-06.cdblog
-echo " -->"`cdbstats -i example-06.cdblog`
+# echo " -->"`cputime -i example-06.cdblog`
 cp example-06.cdbtex lib/.
 cdbcopy.py -i example-06 -t print.ex-06.04 -o example-06-04.cdbcopy
 cdbcopy.py -i example-06 -t print.ex-06.05 -o example-06-05.cdbcopy
--- source/cadabra/exercises/ex-0107.sh
+++ source/cadabra/exercises/ex-0107.sh
@@ -2,7 +2,7 @@
 
 echo "cdblatex.sh ex-0107 ..."
 cdblatex.sh -T -skx -i ex-0107 &> ex-0107.cdblog
-echo " -->"`cdbstats -i ex-0107.cdblog`
+# echo " -->"`cputime -i ex-0107.cdblog`
 cdbcopy.py -i ex-0107 -t print.0107 -o ex-0107.cdbcopy
 pdflatex -halt-on-error -interaction=batchmode ex-0107 &> /dev/null
 # pdflatex -halt-on-error -interaction=batchmode ex-0107 &> /dev/null
